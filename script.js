dee96251c28c37524aeb10cf1b631ca6266f82af
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-03-22 03:09:34 +0900

    ほぼ完成。あとは、レスポンシブを沢山対応させる

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const menuIcon  = document.querySelector('.menu-icon')
 const content = document.querySelector('.content')
+const contactLink = document.querySelector('.navigation a:last-child');
 
 menuIcon.addEventListener('click' , () => {
     content.classList.toggle('navigate')
 })
+
+contactLink.addEventListener('click' , () =>{
+    content.classList.remove('navigate')
+})
+
+//Loader
+const loader = document.querySelector('.loader-wrapper');
+
+window.addEventListener('load' , () =>{
+    const body = document.querySelector('.body');
+    setTimeout(() => {
+        loader.classList.add('hide')
+    } , 6000)
+
+    setTimeout(() => {
+        content.classList.add('show')
+        body.style.overflow='auto'
+    }, 5500)
+})
+//End of Loader
+
 
 const stalker = document.getElementById('mouse-stalker'); 
 document.addEventListener('mousemove', function (e) {
